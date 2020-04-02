d1850f2f6c0ec314e8e2ddaca60d4c33d4198a22
Johann Folta <jfolta@kyruus.com>
2020-04-02 09:31:54 -0400

    actual first python code

---
+++ firstPythonCode.py
@@ -0,0 +1,12 @@
+# Directions: To run...open terminal, change directories (cd) to the folder containing the file ("pythonEdXCourse"), then type "python firstPythonCode.py"
+
+print ('Hello world')
+print ('I can do math:')
+x = 1
+print (x)
+x = x + 1
+print (x)
+x = x + 1
+print (x)
+print ('Good bye~!')
+exit()
