a103c9971c2c8fddfae5d269ff568cf467de49b1
Johann Folta <jfolta@kyruus.com>
2020-04-04 09:37:06 -0400

    python 2 vs 3 with dividing integers

--- checkingTypes.py
+++ checkingTypes.py
@@ -12,3 +12,28 @@ print(d, type(d))
 # float() does not appear to "reassign" value of d; just RETURNS a floating point value for d
 print('using float function to return floating point value of d:')
 print(d, float(d), type(float(d)))
+
+
+# python2 vs python3
+
+# PYTHON3
+# print(9/2)
+# 4.5 (automatically returns floating point)
+
+
+# PYTHON2
+# print(9/2)
+# 4 (stays as integer and loses 0.5)
+
+# thus, solution was to divide floats not integers: 
+# print(9.0/2.0)
+# 4.5
+
+# OR
+
+# print(99/100)
+# 0 (stays as integer and loses 0.99)
+
+# thus, solution was to divide floats not integers: 
+# print(99.0/100.0)
+# 0.99
