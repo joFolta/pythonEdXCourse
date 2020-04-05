06d7281b9be3f2ab8a180c1dea5927eafd17bed6
Johann Folta <jfolta@kyruus.com>
2020-04-05 09:13:00 -0400

    add convertingUserInput program: European-US Elevator Floor Converter

---
+++ convertingUserInput.py
@@ -0,0 +1,8 @@
+# Elevator floor Converter (European -> US)
+
+# In European countries, the ground floor is the 0th floor, 
+# But in the US it is the 1st floor.
+
+inp = input('European floor?')
+usf = int(inp) + 1
+print('Equivalent US floor:', usf)
