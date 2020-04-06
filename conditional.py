cefe635aac033e6497dbed35e6bcdc69c33e948f
Johann Folta <jfolta@kyruus.com>
2020-04-06 08:34:31 -0400

    add conditional

---
+++ conditional.py
@@ -0,0 +1,6 @@
+name = input('Enter your name inside quotes: ')
+
+if name == 'Lucas' :
+  print('Welcome Lucas! So good to see you!')
+else :
+  print("You are not Lucas... I'm waiting for Lucas.")
