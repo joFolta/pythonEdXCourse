c692d2e247f7cae230ce45d0c9c62841d7a125eb
Johann Folta <jfolta@kyruus.com>
2020-04-06 08:20:28 -0400

    add comparisonOperators

---
+++ comparisonOperators.py
@@ -0,0 +1,13 @@
+x = 5
+if x == 5 :
+  print('Equals 5')
+if x > 4 :
+  print('Greater than 4')
+if x >= 5 :
+  print('Great than or equal to 5')
+if x < 6 :
+  print('Less than 6')
+if x <= 5 :
+  print('Less than or equal to 5')
+if x != 6 :
+  print('Does not equal 6')
