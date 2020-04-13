5d2789197ab3029ef260dee096205df0aec79997
Johann Folta <jfolta@kyruus.com>
2020-04-13 08:16:27 -0400

    add return example to function.py

--- function.py
+++ function.py
@@ -9,9 +9,27 @@ def petSound(petType = None):
     print('LOL!')
 
 
-
+# Use try/except block as input() crashes if nothing is entered
 try:
   arg = input("Enter a pet name like dog, cat, or something else to hear it's sound: ")
 except: 
   arg = None
 petSound(arg)
+
+# Variation of function by using return
+def returnPetSound(petType = None):
+  if petType == 'dog':
+    return 'woof!'
+  elif petType == 'cat':
+    return 'meow~'
+  else:
+    return 'LOL!'
+
+def printSentence(petType = None):
+  print("The "+petType+" goes "+returnPetSound(petType)+". ")
+  
+try:
+  newArg = input("To get a sentence, enter a pet type: ")
+except:
+  newArg = 'blank'
+printSentence(newArg)
