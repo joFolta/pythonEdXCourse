dcf65a89920241679e25ba27f4ad3cc5171c8cb9
Johann Folta <jfolta@kyruus.com>
2020-04-22 21:52:00 -0400

    add string array to for_loop

--- for_loop.py
+++ for_loop.py
@@ -1,3 +1,8 @@
 for i in [5, 4, 3, 2, 1]:
   print(i)
 print("BLASTOFF!!!")
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for weekday in weekdays:
+  print(weekday + ' is a work day.')
+print("Now, it's the weekend!")
